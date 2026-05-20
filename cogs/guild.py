125584d04fe355d0c5a8f55c4b3f06739ef34d76
Nikolaus <github@koolych.ru>
2026-05-20 08:21:54 -0800

    Guild info test

--- cogs/guild.py
+++ cogs/guild.py
@@ -1,5 +1,6 @@
 import utils
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 class guild(commands.Cog):
@@ -17,6 +18,17 @@ class guild(commands.Cog):
             return
         
         await debug_channel.send(content=payload)
+    
+    @app_commands.command(
+        name = "guild info",
+        description = "Give it a try!"
+    )
+    async def guild_info(
+        self,
+        interaction: discord.Interaction
+    ):
+        await interaction.response.send_message(content=f"{interaction.guild.owner.mention} (`{interaction.guild.owner_id}`)"
+                                                + "is owner of this server!\n")
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(guild(bot))
